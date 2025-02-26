4bce9aef3105f0d67f9460f0678302461c6413e5
Harvey Lopez <up2118183@myport.ac.uk>
2025-02-26 13:34:22 +0000

    Added: First pieces of the deadline functionality

--- breakdown.md
+++ breakdown.md
@@ -1,9 +1,10 @@
 # **Add User Tasks:**
 
 ## Requires different states for progress levels:
-   * [ ] incl. "unfinished", "in-progress" and "finished"
-   * [ ] could be done by a cyclical function? (display x at count y etc)
+   * [x] ex. "unfinished", "in-progress" and "finished"
+   * [x] could be done by a cyclical function? (display x at count y etc)
    * [ ] use a checkbox and store the state of the box in the DB
+         * store the index of {priorities} instead i.e priorities[2] == "Medium-Priority"
    * [ ] Tasks will contain a {id, name, state, priority, deadline, notification_time, and subtasks}.
 
 ## Tasks should have subtasks:
@@ -13,14 +14,14 @@
    * [ ] All will contain {id, name, state}
 
 ## Should be able to assign priority to tasks:
-   * [ ] incl. "no priority", "low priority", "medium priority", "high priority".
+   * [x] incl. "no priority", "low priority", "medium priority", "high priority".
 	   * [ ] task priority should be stored in the DB with the task
 
 ## A task should have a deadline and a notification:
    * [ ] Should allow the user to enter a deadline date
    * [ ] Should allow the user to enter a notification time 
 		* [ ] (e.g. 1 day/week/month before the deadline)
-   * [ ] both the notification and deadline will be stored under the task in the database (task.deadline, task.notification_time)
+   * [ ] both the notification and deadline will be stored under task in the database (task.deadline, task.notification_time)
 
 
  
--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -8,9 +8,11 @@ export const TodoForm = ({ formAction, initialData }) => {
     errors: {},
   });
 
-  const [priority, setPriority] = useState("undefined");
-  const priorities = ["No Priority", "Low-Priority", "Medium-Priority", "High-Priority"];
+  //setup for cyclePriority
+  const [priority, setPriority] = useState("Set Priority");
+  const priorities = ["No-Priority", "Low-Priority", "Medium-Priority", "High-Priority"];
 
+  // finds which index 'priority' is currently at and cycles it to the next value.
   const cyclePriority = () => {
     const currentIndex = priorities.indexOf(priority);
     const nextIndex = (currentIndex + 1) % priorities.length;
@@ -20,13 +22,21 @@ export const TodoForm = ({ formAction, initialData }) => {
 
   return (
     <>
-      <h1>{initialData.title ? "Update" : "Create"} Post</h1>
+      <h1>{initialData.title ? "Update" : "Create"} Post!</h1>
       <form action={action}>
-        <section>
-          <button htmlFor="priority" type="button" onClick={ cyclePriority }>{ priority }</button>
+
+        <section> {/** upon clicking, the button adheres to cyclePriority, overwriting the default value */}
+          <button 
+            htmlFor="priority" 
+            type="button" 
+            onClick={ cyclePriority }
+            >{ priority }
+          </button>
         </section>
+
         <section>
           <label htmlFor="title">Title</label>
+          <br></br>
           <input
             type="text"
             id="title"
@@ -38,8 +48,10 @@ export const TodoForm = ({ formAction, initialData }) => {
             <p>{formState.errors.title?.join(", ")}</p>
           )}
         </section>
+
         <section>
           <label htmlFor="content">Content</label>
+          <br></br>
           <textarea
             id="content"
             name="content"
@@ -48,13 +60,29 @@ export const TodoForm = ({ formAction, initialData }) => {
           ></textarea>
           {formState.errors.content && (
             <div>
-              {formState.errors.content?.join(", ")} // Display form errors
-              related to the content field
+              {formState.errors.content?.join(", ")} {/**  Display form errors
+              related to the content field */} 
             </div>
           )}
         </section>
+
         <section>
-          <button type="submit">Save</button>
+          <label htmlFor="Deadline"> 
+            Date
+            <input
+              type="date"
+              id="deadline"
+              name="deadline"
+              className="text-black bg-white"
+              defaultValue={ initialData.deadline || getCurrentDate() }
+            /> 
+            
+          </label>
+          
+        </section>
+
+        <section> {/** .log statement being used as a bookmark for later stages of DB posting implementation */}
+          <button type="submit" onClick={console.log("Send To Database!")}>Save</button>
           <Link href="/user/todo">Cancel</Link>
         </section>
       </form>
